5c63b2be74db4b67c9e63b8cabe4ccdcbae9bd74
David Lum/./Affiliates/Samsung Electronics <david.lum91@gmail.com>
2020-09-15 11:52:24 -0400

    Add DELETE batch endpoint

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import os
 import sys
 import time
 import argparse
+import signal
 from typing import List, Optional
 from enum import Enum
 import subprocess
 from multiprocessing import Queue, Process
-from bottle import request, response, post, run, get
+from bottle import request, response, post, run, get, delete
 
 
 class Status(Enum):
@@ -14,16 +15,19 @@ class Status(Enum):
     RUNNING = 'RUNNING'
     FAILED = 'FAILED'
     SUCCEEDED = 'SUCCEEDED'
+    CANCELLED = 'CANCELLED'
 
     def __str__(self):
         return str(self.value)
 
 
-class Result:
-    def __init__(self, batch_id: str, status: Status, log: Optional[str] = None):
+class Batch:
+    def __init__(self, batch_id: str, status: Status, cli_args: List[str], pid: Optional[int] = None, log: Optional[str] = None):
         self.id = batch_id
+        self.cli_args = cli_args
         self.status = status
         self.log = log
+        self.pid = pid
 
     def to_dict(self):
         return {
@@ -31,12 +35,6 @@ class Result:
             'status': str(self.status),
             'log': self.log,
         }
-
-
-class Proc:
-    def __init__(self, batch_id: str, cli_args: List[str]):
-        self.batch_id = batch_id
-        self.cli_args = cli_args
 
 
 FOUR_HUNDRED_ERROR = {
@@ -47,7 +45,7 @@ FOUR_HUNDRED_ERROR = {
 
 @post('/batch/<batch_id:path>')
 def post_batch(batch_id):
-    global step_dict
+    global batch_dict
 
     if not request.json:
         response.status = 400
@@ -56,28 +54,32 @@ def post_batch(batch_id):
     if not cli_args or not isinstance(cli_args, list):
         response.status = 400
         return FOUR_HUNDRED_ERROR
-    if batch_id in step_dict:
+    if batch_id in batch_dict:
         response.status = 400
         return {
             'error': 'ValidationError',
             'error_message': f"Batch `{batch_id}` already exists."
         }
-    result = Result(batch_id, Status.PENDING)
-    step_dict[batch_id] = result
-    process_queue.put(Proc(batch_id, cli_args))
-    return result.to_dict()
+    batch = Batch(batch_id, Status.PENDING, cli_args)
+    batch_dict[batch_id] = batch
+    process_queue.put(batch)
+    return batch.to_dict()
+
+
+def update_batch_dict():
+    global batch_dict
+
+    while not status_queue.empty():
+        status: Batch = status_queue.get()
+        batch_dict[status.id] = status
 
 
 @get('/batch/<batch_id:path>')
 def get_batch(batch_id):
-    global step_dict
-
-    while not status_queue.empty():
-        status: Result = status_queue.get()
-        step_dict[status.id] = status 
-    maybe_step = step_dict.get(batch_id)
-    if maybe_step:
-        return maybe_step.to_dict()
+    update_batch_dict()
+    maybe_batch = batch_dict.get(batch_id)
+    if maybe_batch:
+        return maybe_batch.to_dict()
 
     response.status = 404
     return {
@@ -86,30 +88,58 @@ def get_batch(batch_id):
     }
 
 
+@delete('/batch/<batch_id:path>')
+def delete_batch(batch_id):
+    global batch_dict
+
+    update_batch_dict()
+    maybe_batch: Batch = batch_dict.get(batch_id)
+    if not maybe_batch:
+        response.status = 404
+        return {
+            'error': 'NotFound',
+            'error_message': f"Batch `{batch_id}` not found."
+        }
+
+    if maybe_batch.status == Status.PENDING:
+        response.status = 400
+        return {
+            'error': 'ValueError',
+            'error_message': f"Batch `{batch_id}` still pending, unable to delete."
+        }
+
+    os.kill(maybe_batch.pid, signal.SIGKILL)
+    maybe_batch.status = Status.CANCELLED
+    batch_dict[batch_id] = maybe_batch
+    return maybe_batch.to_dict()
+
+
 @get('/health')
 def get_health():
     return {"status": "OK"}
 
 
-def poll_step(process_q: Queue, status_q: Queue):
+def poll_batch(process_q: Queue, status_q: Queue):
     while True:
         if process_q.empty():
             time.sleep(5)
         else:
-            step: Proc = process_q.get()
-            status_q.put(Result(step.batch_id, Status.RUNNING))
-            result = run_step(step.batch_id, step.cli_args)
+            batch: Batch = process_q.get()
+            result = run_batch(status_queue, batch)
             status_q.put(result)
 
 
-def run_step(batch_id, cli_args):
-    base_log_dir = f'/var/log/steps/{batch_id}'
+def run_batch(status_q: Queue, batch: Batch):
+    base_log_dir = f'/var/log/steps/{batch.id}'
     os.makedirs(base_log_dir, exist_ok=True)
     stderr_path = f'{base_log_dir}/stderr.log'
     stdout_path = f'{base_log_dir}/stdout.log'
     with open(stderr_path, 'w') as stderr, open(stdout_path, 'w') as stdout, \
-            subprocess.Popen(cli_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+            subprocess.Popen(batch.cli_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                              universal_newlines=True, env=os.environ) as proc:
+        batch.status = Status.RUNNING
+        batch.pid = proc.pid
+        status_q.put(batch)
 
         for line in proc.stdout:
             sys.stdout.write(line)
@@ -120,8 +150,9 @@ def run_step(batch_id, cli_args):
 
     return_code = proc.wait()
     with open(stderr_path) as f:
-        status = Status.FAILED if return_code > 0 else Status.SUCCEEDED
-        return Result(batch_id, status, f.read() or None)
+        batch.status = Status.SUCCEEDED if return_code == 0 else Status.FAILED
+        batch.log = f.read() or None
+        return batch
 
 
 if __name__ == '__main__':
@@ -129,10 +160,10 @@ if __name__ == '__main__':
     parser.add_argument('-p', '--port', help='Port to run the service on', type=int, default=8998)
     parser.add_argument("-H", "--host", type=str, help="Which host to bind", default="0.0.0.0")
     args = parser.parse_args()
-    step_dict = {}
+    batch_dict = {}
     process_queue = Queue()
     status_queue = Queue()
-    step_runner_process = Process(target=poll_step, args=(process_queue, status_queue))
-    step_runner_process.daemon = True
-    step_runner_process.start()
+    batch_runner_process = Process(target=poll_batch, args=(process_queue, status_queue))
+    batch_runner_process.daemon = True
+    batch_runner_process.start()
     run(host=args.host, port=args.port)
--- test/test_main.py
+++ test/test_main.py
@@ -10,12 +10,12 @@ def test_post_batch_succeeds():
     assert r.json() == {'id': 'sleep-1', 'status': 'PENDING', 'log': None}
     while r.json()['status'] == 'PENDING':
         time.sleep(3)
-        r = requests.get('http://localhost:8998/batch/sleep-1')
+        r = requests.get(url)
         r.raise_for_status()
     assert r.json()['status'] == 'RUNNING'
     while r.json()['status'] == 'RUNNING':
         time.sleep(3)
-        r = requests.get('http://localhost:8998/batch/sleep-1')
+        r = requests.get(url)
         r.raise_for_status()
     assert r.json()['status'] == 'SUCCEEDED'
 
@@ -27,31 +27,33 @@ def test_post_batch_fails():
     assert r.json() == {'id': 'sleep-2', 'status': 'PENDING', 'log': None}
     while r.json()['status'] == 'PENDING':
         time.sleep(3)
-        r = requests.get('http://localhost:8998/batch/sleep-2')
+        r = requests.get(url)
         r.raise_for_status()
     assert r.json()['status'] == 'FAILED'
 
 
 def test_post_bad_batch_raises():
-    r = requests.post('http://localhost:8998/batch/3', data={'args': ['ls', '-lthra']})
+    url = 'http://localhost:8998/batch/3'
+    r = requests.post(url, data={'args': ['ls', '-lthra']})
     assert r.json() == FOUR_HUNDRED_ERROR
-    r = requests.post('http://localhost:8998/batch/3', json={'not_args': ['ls', '-lthra']})
+    r = requests.post(url, json={'not_args': ['ls', '-lthra']})
     assert r.json() == FOUR_HUNDRED_ERROR
 
 
 def test_double_post_batch_raises():
-    headers = {'content-type': 'application/json'}
-    requests.post('http://localhost:8998/batch/2', json={'args': ['ls', '-lthra']}, headers=headers)
-    r = requests.post('http://localhost:8998/batch/2', json={'args': ['ls', '-lthra']}, headers=headers)
+    url = 'http://localhost:8998/batch/2'
+    requests.post(url, json={'args': ['ls', '-lthra']})
+    r = requests.post(url, json={'args': ['ls', '-lthra']})
     assert r.status_code == 400
     assert r.json()['error_message'] == "Batch `2` already exists."
 
 
 def test_get_batch():
-    r = requests.get('http://localhost:8998/batch/sleep-1')
+    url = 'http://localhost:8998/batch/sleep-1'
+    r = requests.get(url)
     while r.json()['status'] in (str(Status.RUNNING), str(Status.PENDING)):
         time.sleep(2)
-        r = requests.get('http://localhost:8998/batch/sleep-1')
+        r = requests.get(url)
 
     assert r.json() == {'id': 'sleep-1', 'status': 'SUCCEEDED', 'log': None}
 
@@ -66,3 +68,33 @@ def test_health_check():
     r = requests.get('http://localhost:8998/health')
     assert r.status_code == 200
     assert r.json()['status'] == "OK"
+
+
+def test_delete_batch():
+    url = 'http://localhost:8998/batch/sleep-3'
+    r = requests.post(url, json={'args': ['sleep', '20']})
+    r.raise_for_status()
+    assert r.json() == {'id': 'sleep-3', 'status': 'PENDING', 'log': None}
+    r = requests.delete(url)
+    assert r.status_code == 400
+    r = requests.get(url)
+    while r.json()['status'] != 'RUNNING':
+        time.sleep(3)
+        r = requests.get(url)
+        r.raise_for_status()
+
+    r = requests.delete(url)
+    r.raise_for_status()
+    assert r.json() == {'id': 'sleep-3', 'status': 'CANCELLED', 'log': None}
+    while r.json()['status'] == 'CANCELLED':
+        time.sleep(3)
+        r = requests.get(url)
+        r.raise_for_status()
+
+    assert r.json() == {'id': 'sleep-3', 'status': 'FAILED', 'log': None}
+
+
+def test_delete_batch_not_found():
+    r = requests.delete('http://localhost:8998/batch/4')
+    assert r.status_code == 404
+    assert r.json()['error_message'] == "Batch `4` not found."
